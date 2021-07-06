ccf9e2f9fc49d0ecd268fc3c8b9e56019b1fe0e5
pnyuser <ihtishamaptech2@gmail.com>
2021-07-06 18:44:43 +0500

    Third Commit

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    List<String> numbers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Added New Line", Toast.LENGTH_SHORT).show();
+        numbers = new ArrayList<>();
     }
 }
